5e8212d203a4f829356d7d8c1806fe651b36e25a
Loco The Dev <89942527+locothedev@users.noreply.github.com>
2021-12-24 15:09:52 +0100

    Update Coin.vue

--- src/components/Coin.vue
+++ src/components/Coin.vue
@@ -1,6 +1,6 @@
 <template>
 <div v-if="isAuthenticated" style="display: flex; justify-content: center">
- <img :src="tokenInfo?.logo" width="25" height="25"/> <p style="margin: 0px">{{tokenInfo?.symbol}}</p> <p style="margin: 0px">${{price?.toFixed(7)}}</p>
+ <img :src="tokenInfo?.logo" width="25" height="25"/> <p style="margin: 0px;">{{tokenInfo?.symbol}}</p> <p style="margin: 0px;">${{price?.toFixed(7)}}</p>
 </div>
 </template>
 
